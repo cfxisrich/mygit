7802788eace855c99529ae59ff6f045552db7f94
xss <605439507@qq.com>
2021-09-01 11:52:35 +0800

    新加Test01.java

---
+++ src/main/java/com/shuai/Test01.java
@@ -0,0 +1,12 @@
+package com.shuai;
+
+/**
+ * @Description: TODO
+ * @author: scott
+ * @date: 2021年09月01日 11:51
+ */
+public class Test01 {
+    public static void main (String[] args) {
+        System.out.println ("test01....");
+    }
+}
