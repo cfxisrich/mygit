75db1026eab8e1f51ab2495093dcc5253157a294
xss <605439507@qq.com>
2021-09-01 15:14:26 +0800

    修改test01.java

--- src/main/java/com/shuai/Test01.java
+++ src/main/java/com/shuai/Test01.java
@@ -8,5 +8,7 @@ package com.shuai;
 public class Test01 {
     public static void main (String[] args) {
         System.out.println ("test01....");
+        System.out.println ("test02....");
+        System.out.println ("test03....");
     }
 }
